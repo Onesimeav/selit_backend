1312e817a57dcde00ebd6dc5d8fe68fb30ce174d
Onesimeav <avallaonesime@gmail.com>
2024-08-15 01:57:21 +0100

    Initial commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(UrlGenerator $url): void
+    {
+
+        if (env('APP_ENV') == 'production') {
+            $url->forceScheme('https');
+        }
+        ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
+            return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+        });
+    }
+}
