4dc3e0d105b9dbedc313599099796e648b9b8fff
Onesimeav <avallaonesime@gmail.com>
2024-08-19 21:48:55 +0100

    - [x] Selit email verification process - [x] Setup emailing server - [x] send email verification code - [x] Resend a new code - [x] Validate code - [x] lock routes for not-verified users

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Mail;
+use Illuminate\Support\Facades\Mail;
 use App\Mail\TestMail;
 
 class MailController extends Controller
 {
-    public function index()
+    public function index(): \Illuminate\Http\JsonResponse
     {
         Mail::to('exauceavalla@gmail.com')->send(new TestMail([
             'title' => 'The Title',
