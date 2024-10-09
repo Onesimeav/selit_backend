a69fca757011cbdfb1313f25740c487456469c53
Onesimeav <avallaonesime@gmail.com>
2024-10-09 18:17:22 +0100

    Backend API-Buy process : Fix errors - [ ] check and correct the bool values attribution on db - [x] Correct the finish order function and send the reciepe - [x] Add a retrive reciepe function - [x] Backend API- Apply/Modify Template

--- database/migrations/2024_10_01_153225_create_withdrawals_table.php
+++ database/migrations/2024_10_01_153225_create_withdrawals_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->bigInteger('amount');
             $table->unsignedBigInteger('user_id');
-            $table->boolean('status')->default('false');
+            $table->boolean('status')->default(false);
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
         });
