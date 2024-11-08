500c77a8250da82d62fb50b70db532d48da40dea
Onesimeav <avallaonesime@gmail.com>
2024-11-08 21:41:53 +0100

    Backend API-Others - [x] Fix : cors policy error

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->api(prepend: [
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Rakutentech\LaravelRequestDocs\LaravelRequestDocsMiddleware::class,
+            Fruitcake\Cors\CorsService::class,
         ]);
 
         $middleware->alias([
