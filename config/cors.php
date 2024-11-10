38a500afc6b3eeff15581399b2e50a6ba80c624b
Onesimeav <avallaonesime@gmail.com>
2024-11-10 17:43:18 +0100

    Backend API-Others - [x] Fix : cors policy error

--- config/cors.php
+++ config/cors.php
@@ -25,7 +25,7 @@ return [
 
     'allowed_headers' => ['Origin', 'Content-Type', 'X-Auth-Token', 'Cookie','Authorization'],
 
-    'exposed_headers' => [],
+    'exposed_headers' => ['Access-Control-Allow-Origin'],
 
     'max_age' => 0,
 
