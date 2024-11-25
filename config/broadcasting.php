b41bfa60b28a3612c36782d2455e06028a9415e3
Onesimeav <avallaonesime@gmail.com>
2024-11-25 15:37:49 +0100

    Fix: order status change event

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -55,7 +55,7 @@ return [
                 'cluster' =>'eu',
                 'host' => env('PUSHER_HOST') ?: 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
                 'port' => env('PUSHER_PORT', 443),
-                'scheme' => env('PUSHER_SCHEME', 'http'),
+                'scheme' => env('PUSHER_SCHEME', 'https'),
                 'encrypted' => true,
                 'useTLS' => true,
             ],
