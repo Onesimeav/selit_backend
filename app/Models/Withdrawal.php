f5c99c4b5ca95ab4b3a7e423b16871faf62c3faa
Onesimeav <avallaonesime@gmail.com>
2025-03-22 15:06:41 +0100

    fix: user info in withdrawal requests

--- app/Models/Withdrawal.php
+++ app/Models/Withdrawal.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use phpDocumentor\Reflection\Types\Boolean;
 
@@ -45,8 +46,8 @@ class Withdrawal extends Model
         'user'
     ];
 
-    public function user():HasOne
+    public function user():BelongsTo
     {
-        return $this->hasOne(User::class);
+        return $this->belongsTo(User::class);
     }
 }
